b005bf588aa6eddf39d3596ffc6173f65acb2904
Radhe Krishna singh <48623037+rakrisi@users.noreply.github.com>
2023-01-11 21:57:51 +0530

    bug fix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app =Vue.createApp({
         },
         mayUseSpecialAttack(){
             return this.currentRound % 3 !==0;
-        }
+        },
         mayUseHeal(){
             return this.currentRound % 4 !==0;
         }
